26f5e1cd0598965e76221cdc42a932f7bd6d3eab
Roclh <dom-continetnal@yandex.ru>
2022-10-03 21:41:41 +0300

    From new start

--- src/main/java/com/GaysFromITMO/core/Engine.java
+++ src/main/java/com/GaysFromITMO/core/Engine.java
@@ -1,15 +1,5 @@
 package com.GaysFromITMO.core;
 
 public class Engine {
-    public void init(){
 
-    }
-
-    public void startLoop(){
-
-    }
-
-    private void loop(){
-
-    }
 }
--- src/main/java/com/GaysFromITMO/core/Renderer.java
+++ src/main/java/com/GaysFromITMO/core/Renderer.java
@@ -1,39 +1,6 @@
 package com.GaysFromITMO.core;
 
-import com.GaysFromITMO.core.figures.FigureVertexArrayObject;
-import static org.lwjgl.opengl.GL46C.*;
-import org.lwjgl.system.MemoryUtil;
-
-import java.nio.FloatBuffer;
 
 public class Renderer {
-    public static void prepare(){
-        glClear(GL_COLOR_BUFFER_BIT);
-        glClearColor(0.5f, 0.75f, 1.0f, 1);
-    }
 
-    public static void renderVertexArray(FigureVertexArrayObject arrayObject){
-        int vaoId = glGenVertexArrays();
-        glBindVertexArray(vaoId);
-
-        int vboId = glGenBuffers();
-        glBindBuffer(GL_ARRAY_BUFFER, vboId);
-        FloatBuffer buffer = storeDataInDoubleBuffer(arrayObject.getVertexArrayObject());
-        glBufferData(GL_ARRAY_BUFFER, buffer, GL_STATIC_DRAW);
-        glVertexAttribPointer(0,3, GL_FLOAT, false, 0, 0);
-        MemoryUtil.memFree(buffer);
-        glBindBuffer(GL_ARRAY_BUFFER, vboId);
-        glBindVertexArray(vaoId);
-        glEnableVertexAttribArray(0);
-        glDrawArrays(GL_TRIANGLES, 0, arrayObject.count());
-        glDisableVertexAttribArray(0);
-        glBindVertexArray(vaoId);
-    }
-
-    private static FloatBuffer storeDataInDoubleBuffer(float[] data){
-        FloatBuffer buffer = MemoryUtil.memAllocFloat(data.length);
-        buffer.put(data);
-        buffer.flip();
-        return buffer;
-    }
 }
